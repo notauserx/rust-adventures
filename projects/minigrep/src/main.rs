10c4ecde8291a13a3adb78015c32b6cc4b36eb09
notauserx <notauserx@users.noreply.github.com>
2022-12-16 23:35:28 +0600

    return result instead of panic

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     // collectiong the command line arguments in a vector
@@ -8,7 +9,11 @@ fn main() {
     
     //dbg!(args);
 
-    let config = Config::new(&args);
+    let config = Config::build(&args)
+        .unwrap_or_else(|err| {
+            println!("Problem parsing arguments: {err}");
+            process::exit(1);
+        });
 
     println!("Searching for -> {}", config.query);
     println!("In file -> {}", config.file_path);
@@ -26,13 +31,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
