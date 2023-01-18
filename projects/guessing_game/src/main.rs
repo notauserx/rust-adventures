4cd1212734c77a5f86a1c698c4b4a49f58ae6530
notauserx <notauserx@users.noreply.github.com>
2023-01-18 13:25:03 +0600

    with guess struct

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,8 +1,32 @@
 use rand::Rng;
 use std::{cmp::Ordering, io};
 
+const LOWER_LIMIT: i32 = 1;
+const UPPER_LIMIT: i32 = 10;
+
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+pub struct Guess {
+  value: i32,
+}
+
+impl Guess {
+  pub fn new(value: i32) -> Guess {
+    if value < LOWER_LIMIT || value > UPPER_LIMIT {
+      panic!("Guess value must be between {LOWER_LIMIT} and {UPPER_LIMIT}, got {value}.");
+    }
+
+    Guess { value }
+  }
+
+  pub fn value(&self) -> i32 {
+    self.value
+  }
+}
+
 fn main() {
-  let secret_number = rand::thread_rng().gen_range(1..=10);
+  let secret_number = rand::thread_rng().gen_range(LOWER_LIMIT..=UPPER_LIMIT);
+
+  let secret_guess = Guess::new(secret_number);
 
   loop {
     println!("Guess a number");
@@ -14,19 +38,12 @@ fn main() {
 
     println!("You guessed: {}", guess);
 
-    let guess: i32 = match guess.trim().parse() {
-      Ok(num) => num,
+    let guess: Guess = match guess.trim().parse::<i32>() {
+      Ok(num) => Guess::new(num),
       Err(_) => continue,
     };
 
-    if guess < 1 || guess > 100 {
-      println!("The secret number will be between 1 and 10");
-      continue;
-    }
-
-    //.expect("Please type a number.");
-
-    match guess.cmp(&secret_number) {
+    match guess.cmp(&secret_guess) {
       Ordering::Less => println!("too small"),
       Ordering::Greater => println!("too big"),
       Ordering::Equal => {
