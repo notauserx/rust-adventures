179d550ae5bf87e4ac8341b587afc89def34e156
notauserx <notauserx@users.noreply.github.com>
2023-03-28 12:29:50 +0600

    add left and right

--- projects/rparsec/src/lib.rs
+++ projects/rparsec/src/lib.rs
@@ -183,3 +183,19 @@ where
     })
   }
 }
+
+fn left<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Parser<'a, R1>
+where
+  P1: Parser<'a, R1>,
+  P2: Parser<'a, R2>,
+{
+  map(pair(parser1, parser2), |(left, _)| left)
+}
+
+fn right<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Parser<'a, R2>
+where
+  P1: Parser<'a, R1>,
+  P2: Parser<'a, R2>,
+{
+  map(pair(parser1, parser2),|(_, right)| right)
+}
