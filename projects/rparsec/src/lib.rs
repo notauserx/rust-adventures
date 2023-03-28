0aa12017266fe5d9371f23c953253b0ac518a82f
notauserx <notauserx@users.noreply.github.com>
2023-03-28 12:17:07 +0600

    progress

--- projects/rparsec/src/lib.rs
+++ projects/rparsec/src/lib.rs
@@ -90,7 +90,7 @@ fn identifier_parser() {
 // a parser that takes two parsers as input and returns
 // a new parser which parses both of them in order
 // i.e a parser combinator
-fn pair<P1, P2, R1, R2>(parser1: P1, parser2: P2) ->
+fn pair_old<P1, P2, R1, R2>(parser1: P1, parser2: P2) ->
   impl Fn(&str) -> Result<(&str, (R1, R2)), &str>
 where
   P1: Fn(&str) -> Result<(&str, R1), &str>,
@@ -110,7 +110,7 @@ where
 
   #[test]
   fn pair_combinator() {
-      let tag_opener = pair(match_literal("<"), identifier);
+      let tag_opener = pair_old(match_literal("<"), identifier);
       assert_eq!(
           Ok(("/>", ((), "my-first-element".to_string()))),
           tag_opener("<my-first-element/>")
@@ -118,3 +118,68 @@ where
       assert_eq!(Err("oops"), tag_opener("oops"));
       assert_eq!(Err("!oops"), tag_opener("<!oops"));
   }
+
+// enter the functor
+/*
+fn map<P, F, A, B>(parser: P, map_fn: F) -> Result<(&str, B), &str>
+where
+  P: Fn(&str) -> Result<(&str, A), &str>,
+  F: Fn(A) -> B,
+{
+  move |input| 
+    parser(input) 
+      .map(|(next_input, result)| (next_input, map_fn(result)))
+}
+*/
+
+type ParseResult<'a, Output> = Result<(&'a str, Output), &'a str>;
+
+trait Parser<'a, Output> {
+  fn parse(&self, input: &'a str) -> ParseResult<'a, Output>;
+}
+
+impl<'a, F, Output> Parser<'a, Output> for F
+where
+  F: Fn(&'a str) -> ParseResult<Output>,
+{
+  fn parse(&self, input: &'a str) -> ParseResult<'a, Output> {
+    self(input)
+  }
+}
+
+fn map<'a, P, F, A, B>(parser: P, map_fn: F) -> impl Parser<'a, B>
+where
+  P: Parser<'a, A>,
+  F: Fn(A) -> B,
+{
+  move |input|
+    parser.parse(input)
+      .map(|(next_input, result)| (next_input, map_fn(result)))
+}
+
+fn pair_untidy<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Parser<'a, (R1, R2)>
+where
+  P1: Parser<'a, R1>,
+  P2: Parser<'a, R2>,
+{
+  move |input| match parser1.parse(input) {
+    Ok((next_input, result1)) => match parser2.parse(next_input) {
+      Ok((final_input, result2)) => Ok((final_input, (result1, result2))),
+      Err(err) => Err(err),
+    },
+    Err(err) => Err(err),
+  }
+}
+
+fn pair<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Parser<'a, (R1, R2)>
+where
+  P1: Parser<'a, R1>,
+  P2: Parser<'a, R2>,
+{
+  move |input| {
+    parser1.parse(input).and_then(|(next_input, result1)| {
+      parser2.parse(next_input)
+        .map(|(last_input, result2)| (last_input, (result1, result2)))
+    })
+  }
+}
